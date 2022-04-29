2e1360b0ffd3529e4a1db6edc60d3d9aaa2605cc
Brian Nkhata <briannkhata@gmail.com>
2022-04-29 10:08:10 +0200

    Adding extra files and folders to match mvc structure

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
+//require("./app/routes/tutorial.routes.js")(app);
+// set port, listen for requests
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
